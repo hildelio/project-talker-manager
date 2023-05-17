db988eae55d043bb5f99cebbb32e54c335979d36
hildelio <hildelio@gmail.com>
2023-05-17 19:41:04 -0300

    Req 03

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
 
 const app = express();
 
@@ -14,11 +16,13 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// Req 01
 app.get('/talker', async (_req, res) => {
   const talkers = await readFile();
   return res.status(HTTP_OK_STATUS).json(talkers || []);
 });
 
+// Req 02
 app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
   const talkers = await readFile();
@@ -29,6 +33,35 @@ app.get('/talker/:id', async (req, res) => {
     });    
   }
   return res.status(HTTP_OK_STATUS).json(getTalker);
+});
+
+// Req 03
+app.use(bodyParser.json());
+
+function generateToken(length) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let token = '';
+  for (let i = 0; i < length; i += 1) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    token += chars[randomIndex];
+  }
+  return token;
+}
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  // const generateToken = crypto.randomBytes(16, (err, buf) => {
+  //   if (err) {
+  //     console.log(err);
+  //   }
+  //   return buf.toString('hex');
+  // });
+  if (email && password) {
+    const token = generateToken(16);
+    res.status(200).json({ token });
+  } else {
+    res.status(400).json({ error: 'Email and password are required.' });
+  }
 });
 
 app.listen(PORT, () => {
